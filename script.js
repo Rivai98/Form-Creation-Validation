cfe73c840719011cb43f21ca96c645ec50270c55
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-10-05 21:40:35 +0300

    Add DOMContentLoaded

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
-const form = document.getElementById("registration-form");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registration-form");
 
-const feedbackDiv = document.getElementById("form-feedback"); 
+    const feedbackDiv = document.getElementById("form-feedback");
 
-form.addEventListener('submit', function(event){
-    event.preventDefault(); 
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    const usernameInput = document.getElementById("username").value.trim();
-    const emailInput = document.getElementById("email").value.trim(); 
-    const passwordInput = document.getElementById("password").value.trim();
-    
-    // console.log(usernameInput); 
-    // console.log(emailInput); 
-    // console.log(passwordInput); 
+        const usernameInput = document.getElementById("username").value.trim();
+        const emailInput = document.getElementById("email").value.trim();
+        const passwordInput = document.getElementById("password").value.trim();
 
-
-    let isValid = true; 
-    let messages = []; 
-
-    if (usernameInput.length < 3 ){
-        isValid= false ; 
-        messages.push("Write username longer please!");
-         
-    }
-
-    if(!emailInput.includes("@") || !emailInput.includes(".")){
-        isValid = false;
-        messages.push("Email must include both '@' and '.' characters.");
-    }
-
-    if(passwordInput.length <  8 ){
-        isValid=false; 
-        messages.push("Password is too short please add more characters"); 
-
-    } 
-
-    if(isValid){
-        feedbackDiv.textContent = "Registration successful!"; 
-        feedbackDiv.style.color = "#28a745"; 
-    }else {
-        feedbackDiv.innerHTML = messages.join("<br>"); 
-        feedbackDiv.style.color = "#dc3545"; 
-    }
+        // console.log(usernameInput); 
+        // console.log(emailInput); 
+        // console.log(passwordInput); 
 
 
+        let isValid = true;
+        let messages = [];
 
+        if (usernameInput.length < 3) {
+            isValid = false;
+            messages.push("Write username longer please!");
 
+        }
 
-    
+        if (!emailInput.includes("@") || !emailInput.includes(".")) {
+            isValid = false;
+            messages.push("Email must include both '@' and '.' characters.");
+        }
+
+        if (passwordInput.length < 8) {
+            isValid = false;
+            messages.push("Password is too short please add more characters");
+
+        }
+
+        if (isValid) {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745";
+        } else {
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545";
+        }
 
 
 
@@ -55,4 +50,12 @@ form.addEventListener('submit', function(event){
 
 
 
-}); 
+
+
+
+
+
+
+    });
+});
+
